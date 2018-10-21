ee2ee27f43a7d36994fd0418ccfdf58ebe8d2615
Andrew Johnson <ajohnson.uva@gmail.com>
2018-10-21 19:25:21 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,8 +2,8 @@ def my_select(collection)
   i = 0
   new_array = Array.new
   while i < collection.size
-    if yield(array[i])
-      new_array << array[i]
+    if yield(collection[i])
+      new_array << collection[i]
     end
     i += 1
   end
